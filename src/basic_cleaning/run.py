620ef26d87529b5d5a05fb580a66896aad4fb6e0
Corey Reed <reed.corey@gmail.com>
2024-03-09 13:44:36 -0500

    Added code to filter points outside target geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,8 +32,10 @@ def go(args):
     idx = df['price'].between(float(args.min_price), float(args.max_price))
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
-    # TODO: add code to fix the issue happened when testing the model
-    
+
+    # Drops rows in dataset that are not the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
     logger.info('Saving and exporting cleaned data.')
@@ -45,9 +47,8 @@ def go(args):
     )
     artifact.add_file('clean_sample.csv')
     run.log_artifact(artifact)
-    
-# TODO: In the code below, fill in the data type for each argumemt. The data type should be str, float or int. 
-# TODO: In the code below, fill in a description for each argument. The description should be a string.
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="A very basic data cleaning")
